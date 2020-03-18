b2fd31b388bf57c5b22cb409e8caefd95e9db4f2
Luis Leyva <lmorales6@ucol.mx>
2020-03-18 15:25:57 -0600

    ya casi

--- tarea-02-encapsulacion-y-herencia-el-restaurante-LuisEdoardo/main.js
+++ tarea-02-encapsulacion-y-herencia-el-restaurante-LuisEdoardo/main.js
@@ -3,10 +3,23 @@ import Fecha from "./fecha.js"
 import Tiempo from "./tiempo.js"
 import Direccion from "./direccion.js"
 import Cliente from "./cliente.js"
+import ClienteFrecuente from "./cliente-frecuente.js"
 import Producto from "./producto.js"
 import ElementoPedido from "./elemento-pedido.js"
 import Pedido from "./pedido.js"
 import Restaurante from "./restaurante.js"
+
+const cliente1 = new Cliente ({
+    nombre: "Alvara Cruz Montes",
+    direccion: new Direccion("Valle De Desesperanza","345","", "Nuevo Terreno", "28048", "Colima", "Colima"),
+    telefono: "3126789345"
+})
+
+const cliente2 = new Cliente ({
+    nombre: "Luis Edoardo Morales Leyva",
+    direccion: new Direccion("Hacienda del Carmen","456","", "Ostras", "278935", "Colima", "Colima"),
+    telefono: "3124537823"
+})
 export default class Main {
 
     constructor(){
@@ -14,14 +27,14 @@ export default class Main {
     }
 
     pruebaTiempo(){
-        let horas = new Tiempo(5,44,"pm")
+        let horas = new Tiempo(5, 44, "pm")
         console.log("---------Tiempo--------")
         console.log(horas.getFormato12())
         console.log(horas.getFormato24())
     }
 
     pruebaFecha(){
-        let fecha = new Fecha(5,4,2017)
+        let fecha = new Fecha(1 , 2, 2000)
         console.log("----------Fecha---------")
         console.log(fecha.getAños())
         console.log(fecha.getMeses())
@@ -38,12 +51,12 @@ export default class Main {
     }
 
     pruebaDireccion(){
-        let calle = "Hacienda el Cortijo"
-        let numExt = "601"
-        let numInt = "200"
-        let colonia = "Real Centenario"
-        let CP = "28984"
-        let municipio = "Villa de Alvarez"
+        let calle = "Montes Himalaya"
+        let numExt = "1215"
+        let numInt = ""
+        let colonia = "Nuevo Milenio"
+        let CP = "28048"
+        let municipio = "Colima"
         let ciudad = "Colima"
         let direccion = new Direccion(calle,numExt,numInt,colonia,CP,municipio,ciudad)
         console.log("--------Direccion---------")
@@ -52,16 +65,29 @@ export default class Main {
     }
 
     pruebaCliente(){
-        let nombre = "Alberto Esparza Gomez Rodriguez"
-        let direccion = new Direccion("Laguna del Calabozo","442","", "Villas Bugambilia", "28956", "Villa de Alvarez", "Colima")
-        let telefono = "312245567"
-        let cliente = new Cliente(nombre,direccion,telefono)
+        let cliente = new Cliente ({
+            nombre: "Mariana Ramos Ibarra",
+            direccion: new Direccion("Justo Sierra","302","", "Centro", "290867", "Colima", "Colima"),
+            telefono: "3121263829"
+        })
         console.log("----------Cliente---------")
         console.log(cliente.getPerfil())
     }
 
+    pruebaClienteFrecuente(){
+        let clienteFrecuente1 = new ClienteFrecuente({
+            numeroCliente: 321,
+            fechaRegistro: new Fecha(2,4,2010),
+            nombre: ("Maria Fernanda Garcia Solis"),
+            direccion: new Direccion ("Nicolas Bravo", "1513", "","Las Quintas","256782","Villa de Alvarez", "Colima"),
+            telefono: "3125672345"
+        })
+        console.log('-----Cliente Frecuente-----')
+        console.log(clienteFrecuente1.getPerfil())
+    }
+
     pruebaProducto(){
-        let nombre = "Risotto con champiñones"
+        let nombre = "Panini"
         let precio = new Precio(150)
         let producto = new Producto(nombre,precio)
         console.log("--------Producto---------")
@@ -69,44 +95,105 @@ export default class Main {
     }
 
     pruebaElementoPedido(){
-        let cantidad = 2
-        let producto = new Producto("Pasta con allioli", new Precio(130))
-        let elemento = new ElementoPedido(cantidad,producto)
+        let elementoPedido = new ElementoPedido({
+            cantidad:2,
+            producto: new Producto("Lasagna", new Precio(130))
+        })
         console.log("--------Elemento Pedido--------")
-        console.log(elemento.getDescripcionB())
+        console.log(elementoPedido.getDescripcionB())
     }
     
     pruebaPedido(){
-        let cantidad1 = 1
-        let cantidad2 = 3
-        this.elemento1 = new ElementoPedido(cantidad1,new Producto("Hamburguesa Vegetariana", new Precio(110)))
-        this.elemento2 = new ElementoPedido(cantidad2,new Producto("Filete Mignon", new Precio(200)))
-        let pedido = new Pedido(new Fecha(1,5,2020),new Tiempo(1,22,"pm"),new Cliente("Ramon Castro Lopez"),new Direccion ("Miguel Hidalgo","222","","Vista Hermosa","298345","Colima","Colima"), "3124456446")
+        let pedido1 = new Pedido({
+            numeroPedido: 1,
+            fecha: new Fecha(1,3,2020),
+            hora: new Tiempo(1,44,"pm"),
+            cliente: cliente1
+        })
+        let elementoPedido1 = new ElementoPedido({
+            cantidad:2,
+            producto: new Producto("Lasagna", new Precio(130))
+        })
+        let elementoPedido2 = new ElementoPedido({
+            cantidad:3,
+            producto: new Producto("Filete Mignon", new Precio(200))
+        })
         console.log("-------Pedido-------")
-        pedido.agregarElemento(this.elemento1)
-        pedido.agregarElemento(this.elemento2)
-        console.log("Hay una cantidad de " + pedido.getNumeroElementos() + " productos distintos")
-        console.log("Hay un total de " + pedido.getNumeroProductos()+ " productos") 
-        pedido.listarElementos()
-        console.log(pedido.getResumen())
-        console.log("El costo total es de: " + pedido.getCostoTotal())
+        pedido1.agregarElemento(elementoPedido1)
+        pedido1.agregarElemento(elementoPedido2)
+        console.log("Hay una cantidad de " + pedido1.getNumeroElementos() + " productos distintos")
+        console.log("Hay un total de " + pedido1.getNumeroProductos()+ " productos") 
+        pedido1.listarElementos()
+        console.log(pedido1.getResumen())
+        console.log("El costo total es de: " + pedido1.getCostoTotal())
     }
 
     pruebaRestaurante(){
-        let cantidad1 = 2
-        let cantidad2 = 4
-        this.producto1 = new ElementoPedido(cantidad1,new Producto("Hamburguesa Angus", new Precio(130))) 
-        this.producto2 = new ElementoPedido(cantidad2, new Producto("Pizza Napoletana", new Precio(120)))
-        let pedido1 = new Pedido(new Fecha(1,3,2020),new Tiempo(5,32,"pm"),new Cliente("Alfredo Gomez Tuercas"), new Direccion("Hacienda Venustiano","427","","Calaboide","294578","Guadalajara", "Jalisco"), "312231321")
-        let restaurante = new Restaurante("Ristorante L'eterna cima", "312454467", new Direccion("Lapiz Lazuli","567","","Calabozo","294578","Guadalajara", "Jalisco"))
-        restaurante.registrarProducto(this.producto1)
-        restaurante.registrarProducto(this.producto2)
-        pedido1.agregarElemento(this.producto1)
-        pedido1.agregarElemento(this.producto2)
-        restaurante.registrarPedido(pedido1)
         console.log('------Restaurante------')
-        restaurante.listarProductos()
+        let restaurante = new Restaurante ({
+            nombre:"La dulse vida",
+            telefono:"33024567",
+            direccion: new Direccion("Rio Batacudea","714","","Los Huisaches","267894","Culiacan", "Sinaloa")
+        })
+        let elementoPedido1 = new ElementoPedido({
+            cantidad:2,
+            producto: new Producto("Panini", new Precio(130))
+        })
+        let elementoPedido2 = new ElementoPedido({
+            cantidad:4,
+            producto: new Producto("Filete Mignon", new Precio(200))
+        })
+        let pedido1 = new Pedido({
+            numeroPedido: 1,
+            fecha: new Fecha(1, 3, 2020),
+            hora: new Tiempo(1, 44, "pm"),
+            cliente: cliente1
+        })
+
+        let pedido2 = new Pedido({
+            numeroPedido: 2,
+            fecha: new Fecha(1, 3, 2020),
+            hora: new Tiempo(1, 44, "pm"),
+            cliente: cliente2
+        })
+
+        
+        //Registrar Pedidos
+        restaurante.registrarPedido(pedido1)
+        restaurante.registrarPedido(pedido2)
+        //Registrar Productos
+        restaurante.registrarProducto(elementoPedido1)
+        restaurante.registrarProducto(elementoPedido2)
+        pedido1.agregarElemento(elementoPedido1)
+        pedido2.agregarElemento(elementoPedido2)
         restaurante.listarPedidos()
+        restaurante.listarProductos()
+        console.log("")
+
+        
+        console.log("----Buscar Pedido----")
+        console.log(restaurante.buscarPedido(pedido1))
+        console.log(restaurante.buscarPedido(pedido2))
+        console.log("")
+
+        console.log("----Buscar Indice Pedidos----")
+        console.log(restaurante._encontrarIndicePedido(pedido1))
+        console.log(restaurante._encontrarIndicePedido(pedido2))
+        console.log("")
+
+        console.log("----Eliminar Pedido----")
+        console.log(restaurante.eliminarPedido(pedido1))
+        restaurante.listarPedidos()
+        restaurante.listarProductos()
+        console.log("")
+
+        console.log("----Modificar Pedido----")
+        console.log(restaurante.modificarPedido(pedido2,pedido1))
+        restaurante.listarPedidos()
+        
+        
+        
+        
         
         
     }
@@ -118,6 +205,7 @@ tester.pruebaFecha();
 tester.pruebaPrecio();
 tester.pruebaDireccion();
 tester.pruebaCliente();
+tester.pruebaClienteFrecuente();
 tester.pruebaProducto();
 tester.pruebaElementoPedido();
 tester.pruebaPedido();
--- tarea-02-encapsulacion-y-herencia-el-restaurante-LuisEdoardo/restaurante.js
+++ tarea-02-encapsulacion-y-herencia-el-restaurante-LuisEdoardo/restaurante.js
@@ -2,34 +2,65 @@ import Direccion from "./direccion.js"
 import Pedido from "./pedido.js"
 export default class Restaurante{
 
-    constructor(nombre,telefono,direccion){
-        this.nombre = nombre;
-        this.telefono = telefono;
-        this.direccion = direccion;
-        this.productos = []
-        this.pedidos = []
+    constructor({nombre,telefono,direccion}){
+        this._nombre = nombre;
+        this._telefono = telefono;
+        this._direccion = direccion;
+        this._productos = [];
+        this._pedidos = [];
 
     }
     
     registrarProducto(producto){  
-        this.productos.push(producto)
+        return `${this._productos.push(producto)}`
     }
 
+
     registrarPedido(pedido){
-        this.pedidos.push(pedido)
+        this._pedidos.push(pedido)
 
     }
     
     listarPedidos(){
-        this.pedidos.forEach((pedido,i) =>{
+        this._pedidos.forEach((pedido,i) =>{
             console.log(`Pedido (${i+1}) ${pedido.getResumen()}`)
         })
     }
 
+    buscarPedido(pedido){
+        let result = this._pedidos.find(order => order.esIgualA(pedido))
+        return result;
+    }
+
+    _encontrarIndicePedido(pedido){
+        let indice = this._pedidos.findIndex(orders => orders.esIgualA(pedido))
+        return indice;
+    }
+
+    eliminarPedido(pedido){
+        let indice = this.buscarPedido(pedido)
+        if (indice < 0 ){
+            return false;
+        }
+        this._pedidos.splice(indice,1)
+        return true; 
+    }
+
+    modificarPedido(pedido,nuevoPedido){
+        let indice = this._encontrarIndicePedido(pedido);
+        if (indice < 0){
+            return false;
+        }
+        this._pedidos.splice(indice,1,nuevoPedido)
+        return true;
+    }
+
     listarProductos(){
-        this.productos.forEach((producto) =>{
-            console.log('Producto:', producto.getDescripcionB())
-        })
+        console.log("Listado de productos")
+        this._productos.forEach(producto =>{
+            console.log(producto.getDescripcionB())
+            
+        }) 
     }
        
 }
